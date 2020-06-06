af4e7a88a172df88f81aca6968ebdf806ac3776a
vishalSBhat <vishalbhat97@gmail.com>
2020-06-06 18:23:16 +0530

    Updates

---
+++ precache-manifest.2b93792e07f1de476e303a1667691da7.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f863431cd2445470f5612dfd884bc637",
+    "url": "/solitude-loyalty/index.html"
+  },
+  {
+    "revision": "8abaa9a0a1a4ba07cf82",
+    "url": "/solitude-loyalty/static/css/2.d5dfffcb.chunk.css"
+  },
+  {
+    "revision": "aa1b3e86200c9c6569ed",
+    "url": "/solitude-loyalty/static/css/main.0c4687bd.chunk.css"
+  },
+  {
+    "revision": "8abaa9a0a1a4ba07cf82",
+    "url": "/solitude-loyalty/static/js/2.ac3da586.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/solitude-loyalty/static/js/2.ac3da586.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa1b3e86200c9c6569ed",
+    "url": "/solitude-loyalty/static/js/main.6fdb9471.chunk.js"
+  },
+  {
+    "revision": "a7cc159596a7b4872178",
+    "url": "/solitude-loyalty/static/js/runtime-main.cf1405b1.js"
+  },
+  {
+    "revision": "a649096d86d465e4d1062d68a99ce570",
+    "url": "/solitude-loyalty/static/media/background.a649096d.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/solitude-loyalty/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/solitude-loyalty/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/solitude-loyalty/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/solitude-loyalty/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/solitude-loyalty/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "f8dd6ad9a12929e35f72af0d3410d1f3",
+    "url": "/solitude-loyalty/static/media/logo.f8dd6ad9.png"
+  }
+]);
